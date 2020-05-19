f36afcc98813bce97733415f775cab64542d8374
Akshay <kulkarniakshay82@gmail.com>
2020-05-19 17:32:21 +0530

    programs

---
+++ Project2/src/domain/CustomerSimulator.java
@@ -0,0 +1,12 @@
+package domain;
+
+import model.Customer;
+
+public class CustomerSimulator
+{
+	public static void main(String[] args)
+	{
+		Customer c1=new Customer("SACHIN","MUMBAI");
+		c1.displayCustomerInfo();
+	}
+}
